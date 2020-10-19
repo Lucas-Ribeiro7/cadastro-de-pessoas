93df1cdb6cbfba82a5a3b9bc30442de4f82a106c
Lucas Ribeiro <caetanolucasribeiro@gmail.com>
2020-10-19 11:32:54 -0300

    Update

---
+++ Código Fonte/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css">
+    <title>Cadastro</title>
+</head>
+<body>
+    <section id="Direita">
+        <form action="">
+            <h2>Cadastrar Pessoa</h2>
+            <label>Nome</label>
+            <input type="text" name="nome">
+            <label>Telefone</label>
+            <input type="text" name="telefone">
+            <label>Email</label>
+            <input type="text" name="email">
+            
+        </form>
+    </section>
+    <section>
+    
+    </section id="Esquerda"> 
+</body>
+</html>
