a5efecd3d65aa737acb4bbe9a525f6d44aef7632
Lucas Ribeiro <caetanolucasribeiro@gmail.com>
2020-10-19 21:25:01 -0300

    Update

--- Código Fonte/index.php
+++ Código Fonte/index.php
@@ -7,20 +7,33 @@
     <title>Cadastro</title>
 </head>
 <body>
-    <section id="Direita">
+    <section id="esquerda">
         <form action="">
-            <h2>Cadastrar Pessoa</h2>
+            <h2>CADASTRO DE PESSOAS</h2>
             <label>Nome</label>
             <input type="text" name="nome">
             <label>Telefone</label>
             <input type="text" name="telefone">
             <label>Email</label>
             <input type="text" name="email">
-            
+            <input type="submit" value="Cadastrar">
         </form>
     </section>
-    <section>
+    <section id="direita">
+        <table>
+            <tr>
+                <td>Nome</td>
+                <td>Telefone</td>
+                <td colspan="2">Email</td> <!--Colspan faz com ele o <td> oculpe dois espaco!-->
+            </tr>
+            <tr>
+                <td>Teste</td>
+                <td>Teste</td>
+                <td>Teste</td>
+                <td><a href="">Editar</a><a href="">Excluir</a></td>
+            </tr>
+        </table>
     
-    </section id="Esquerda"> 
+    </section> 
 </body>
 </html>
